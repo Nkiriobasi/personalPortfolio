181207edcbd684f901e6c3ec2e836d454dfcd203
Nkiriobasi <obasinkiri@gmail.com>
2023-03-17 12:46:58 +0100

    first commit

---
+++ pages/flutterwave.jsx
@@ -0,0 +1,78 @@
+import Image from 'next/image';
+import React from 'react';
+import flutterwaveImg from '../public/assets/projects/flutterwave.png'
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+const flutterwave = () => {
+  return (
+    <div className='w-full overflow-x-hidden'>
+      <div className='w-screen h-[50vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={flutterwaveImg}
+          alt='/'
+        />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>Flutterwave UI Clone</h2>
+          <h3>React JS / Sass / Tailwind CSS / React-router</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            This appplication was built using REACT.JS and is styled with
+            Sass and Tailwind. The application is hosted using netlify. This is a mobile 
+            responsive recreation of Flutterwave website. and used react router for client-side 
+            routing.
+          </p>
+          <a
+            href='https://github.com/Nkiriobasi/flutterwave_clone/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+          </a>
+          <a
+            href='https://flutterwave-clone-nk.netlify.app/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4'>Demo</button>
+          </a>
+
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1 '>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Sass
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Tailwind
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React-router
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'>
+          <p className='underline cursor-pointer'>Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default flutterwave;
---
+++ pages/winamp.jsx
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import React from 'react';
+import winampImg from '../public/assets/projects/winamp.png'
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+const winamp = () => {
+  return (
+    <div className='w-full overflow-x-hidden'>
+      <div className='w-screen h-[50vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={winampImg}
+          alt='/'
+        />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>Winamp UI Clone</h2>
+          <h3>React JS / Sass / React Router</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            This appplication was built using REACT.JS and is styled with
+            Sass. The application is hosted using netlify. This is a mobile 
+            responsive recreation of Winamp website. and used react router for client-side 
+            routing.
+          </p>
+          <a
+            href='https://github.com/Nkiriobasi/winamp-clone'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+          </a>
+          <a
+            href='https://winamp-clone-nk.netlify.app/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4'>Demo</button>
+          </a>
+
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1 '>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Sass
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React Router
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'>
+          <p className='underline cursor-pointer'>Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default winamp;
